91d59f97d33d35d3ca15632813432af66855e8fb
Olga <14594349+olgazju@users.noreply.github.com>
2023-12-18 00:15:17 +0200

    added generate.py code

---
+++ capstione_project_1/generate.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException, Request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+from fastapi.encoders import jsonable_encoder
+import numpy as np
+import pickle
+
+app = FastAPI()
+
+# Load the model
+model = load_model('model/my_lyric_model.h5')
+
+# Load max_sequence_len in your FastAPI service
+with open('model/max_sequence_len.txt', 'r') as f:
+    max_sequence_len = int(f.read())
+
+# Load the tokenizer
+with open('model/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def sample_with_temperature(probabilities, temperature=1.0):
+    # Adjust the probabilities with temperature
+    probabilities = np.asarray(probabilities).astype('float64')
+    probabilities = np.log(probabilities + 1e-10) / temperature  # Adding a small constant to avoid division by zero
+    exp_probs = np.exp(probabilities)
+    probabilities = exp_probs / np.sum(exp_probs)
+
+    # Sample the next word based on the adjusted probabilities
+    choices = range(len(probabilities))  # This should be the range of your vocabulary
+    next_word = np.random.choice(choices, p=probabilities)
+
+    return next_word
+
+def generate_with_temperature(model, seed_text, temperature=1.0):
+
+    next_words = 100
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+
+        probabilities = model.predict(token_list, verbose=0)[0]  # Get softmax probabilities
+
+        # Use temperature to adjust the probabilities and sample the next word
+        predicted = sample_with_temperature(probabilities, temperature=temperature)  # Adjust the temperature as needed
+
+        # Convert the predicted token to a word and update the seed text
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    
+    return seed_text
+
+@app.post("/generate")
+async def generate(request: Request):
+    data = await request.json()
+    print("data", data)
+
+    # Generate text using the model
+    result = generate_with_temperature(model, data['prompt'], 0.6)
+    return jsonable_encoder({"generation": result})
+
+# Run the server: uvicorn generate:app --reload
---
+++ capstione_project_1/train.py
@@ -0,0 +1,156 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import Callback, EarlyStopping
+
+df = pd.read_parquet("cleaned.parquet")
+
+combined_lyrics = ' '.join(df['lyric_clean'])
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([combined_lyrics])
+total_words = len(tokenizer.word_index) + 1
+input_sequences = []
+for line in df['lyric_clean']:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+max_sequence_len = max(len(x) for x in input_sequences)
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+predictors, label = input_sequences[:,:-1], input_sequences[:,-1]
+label = to_categorical(label, num_classes=total_words)
+
+# Splitting Data
+X_train, X_val, y_train, y_val = train_test_split(predictors, label, test_size=0.2, random_state=42)
+
+# Perplexity Calculation
+class Perplexity(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        cross_entropy = logs.get('loss')
+        perplexity = np.exp(cross_entropy)
+        print(f' - perplexity: {perplexity}')
+
+def viz_metrics(history):
+    # Visualizing Training and Validation Metrics
+    plt.figure(figsize=(12, 6))
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend()
+    plt.show()
+
+def train(model):
+  earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=1, mode='auto', restore_best_weights=True)
+  history = model.fit(X_train, y_train, epochs=50, validation_data=(X_val, y_val), callbacks=[earlystop, Perplexity()])
+  return history
+
+def generate(model, seed_text):
+  next_words = 100  # Number of words you want to generate
+
+  for _ in range(next_words):
+      # Tokenize the current seed text
+      token_list = tokenizer.texts_to_sequences([seed_text])[0]
+
+      # Pad the sequence
+      token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+
+      # Predict the next word (as probabilities)
+      probabilities = model.predict(token_list, verbose=0)
+
+      # Get the index of the most likely next word
+      predicted = np.argmax(probabilities, axis=-1)[0]
+
+      # Convert the predicted token to a word
+      output_word = ""
+      for word, index in tokenizer.word_index.items():
+          if index == predicted:
+              output_word = word
+              break
+
+      # Update the seed text
+      seed_text += " " + output_word
+
+  return seed_text
+
+
+def sample_with_temperature(probabilities, temperature=1.0):
+    # Adjust the probabilities with temperature
+    probabilities = np.asarray(probabilities).astype('float64')
+    probabilities = np.log(probabilities + 1e-10) / temperature  # Adding a small constant to avoid division by zero
+    exp_probs = np.exp(probabilities)
+    probabilities = exp_probs / np.sum(exp_probs)
+
+    # Sample the next word based on the adjusted probabilities
+    choices = range(len(probabilities))  # This should be the range of your vocabulary
+    next_word = np.random.choice(choices, p=probabilities)
+
+    return next_word
+
+def generate_with_temperature(model, seed_text, temperature=1.0):
+
+    next_words = 100
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+
+        probabilities = model.predict(token_list, verbose=0)[0]  # Get softmax probabilities
+
+        # Use temperature to adjust the probabilities and sample the next word
+        predicted = sample_with_temperature(probabilities, temperature=temperature)  # Adjust the temperature as needed
+
+        # Convert the predicted token to a word and update the seed text
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    
+    return seed_text
+
+# Building the Model
+model = Sequential()
+model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
+#model1.add(LSTM(150, return_sequences=True))
+model.add(LSTM(150))
+model.add(Dense(total_words, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+
+history = train(model)
+viz_metrics(history)
+
+generate_with_temperature(model, 'ring any bells', 0.3)
+
+# Save the model
+model.save('my_lyric_model.h5')
+
+import pickle
+
+# Assume 'tokenizer' is your Keras Tokenizer instance
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# After defining max_sequence_len during training
+with open('max_sequence_len.txt', 'w') as f:
+    f.write(str(max_sequence_len))
+
+
